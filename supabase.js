812c516f251df2326e8ff439ceae858ab6094426
Umut Şen <umutsen2662@gmail.com>
2024-11-06 00:54:46 +0200

    p

--- supabase.js
+++ supabase.js
@@ -3,6 +3,8 @@ const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
 );
 
+const userid = null;
+
 // Sign Out
 async function signOut() {
     const { error } = await supabase.auth.signOut();
@@ -21,6 +23,7 @@ async function checkSession() {
 
     if (data) {
         console.log("User signed in:", data.session);
+        userid = data.session.user.id;
         if (window.location.pathname !== "/Web-App/") {
             window.location.href = "/Web-App";
         }
