9f588f68913097293607c5c42ea06c73fdf99504
Umut Şen <umutsen2662@gmail.com>
2024-11-06 13:06:32 +0200

    t

--- supabase.js
+++ supabase.js
@@ -2,7 +2,6 @@ const supabase = createClient(
     "https://gpprfxmjjjowyuqgvwzr.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
 );
-let userid = null;
 
 // Sign Out
 async function signOut() {
@@ -16,25 +15,17 @@ async function signOut() {
     }
 }
 
-// Check if a user is signed in (Session Management)
+// Check if a user is signed in
 async function checkSession() {
     const { data, error } = await supabase.auth.getSession();
 
     if (data.session) {
         console.log("User signed in:", data.session.user.email);
-        userid = data.session.user.id;
-        if (window.location.pathname !== "/Web-App/") {
-            window.location.href = "/Web-App";
-        }
+        return data.session.user.id;
     } else if (error) {
         console.error("Session Check Error:", error.message);
     } else {
         console.log("No user signed in.");
-        if (window.location.pathname == "/Web-App/") {
-            window.location.href = "/Web-App/auth/signin.html";
-        }
     }
+    return null;
 }
-
-// Check session on page load
-document.addEventListener("DOMContentLoaded", checkSession);
