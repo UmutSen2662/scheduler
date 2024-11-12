062e788e29ccf34bf4e23c1d72949cb4afd8f1b2
Umut Şen <umutsen2662@gmail.com>
2024-11-12 16:20:44 +0200

    experimental no signed in user test

--- supabase.js
+++ supabase.js
@@ -3,19 +3,16 @@ const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
 );
 
-// Sign Out
-async function signOut() {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-        alert("Error signing out: " + error.message);
-    } else {
-        const userid = await checkSession();
-        if (userid) {
-            alert("Could not Sign-out");
-        } else {
-            window.location.href = "/Scheduler/auth/signin.html";
+// Sign In/Out
+async function signInOut() {
+    const userid = await checkSession();
+    if (userid) {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            alert("Error signing out: " + error.message);
         }
     }
+    window.location.href = "/Scheduler/auth/signin.html";
 }
 
 // Check if a user is signed in
