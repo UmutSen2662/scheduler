e52275c810940d270475209fd0226cdc0c7ddce9
Umut Şen <umutsen2662@gmail.com>
2024-11-16 23:21:38 +0200

    trying the older import method

--- supabase.js
+++ supabase.js
@@ -1,3 +1,5 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 // Check if a user is signed in
 async function checkSession() {
     const { data, error } = await window.supabase.auth.getUser();
@@ -14,17 +16,11 @@ async function checkSession() {
 
 // If offline set supabase and userid to null else check session
 if (navigator.onLine) {
-    import("https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm")
-        .then(async ({ createClient }) => {
-            window.supabase = await createClient(
-                "https://gpprfxmjjjowyuqgvwzr.supabase.co",
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
-            );
-            window.userid = await checkSession();
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    window.supabase = await createClient(
+        "https://gpprfxmjjjowyuqgvwzr.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
+    );
+    window.userid = await checkSession();
 } else {
     window.supabase = null;
     window.userid = null;
