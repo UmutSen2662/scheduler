14fe81291302b2f7d9e638f677d6c8fc7797735c
Umut Şen <umutsen2662@gmail.com>
2024-11-16 21:43:55 +0200

    bugfix on supabase

--- supabase.js
+++ supabase.js
@@ -3,21 +3,7 @@ const supabase = await createClient(
     "https://gpprfxmjjjowyuqgvwzr.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
 );
-
-// Sign In/Out
-async function signInOut() {
-    const userid = await checkSession();
-    if (userid) {
-        const { error } = await supabase.auth.signOut();
-        if (error) {
-            alert("Error signing out: " + error.message);
-        } else {
-            window.location.href = "/Scheduler/"; // redirect to home page
-            return;
-        }
-    }
-    window.location.href = "/Scheduler/auth/signin.html";
-}
+window.supabase = supabase;
 
 // Check if a user is signed in
 async function checkSession() {
