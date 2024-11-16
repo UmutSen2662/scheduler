19724d49545785d3a5be1662ed9abb4b04052bba
Umut Şen <umutsen2662@gmail.com>
2024-11-16 21:39:38 +0200

    changed how supabase works

--- supabase.js
+++ supabase.js
@@ -1,4 +1,8 @@
-const supabase = window.supabase;
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabase = await createClient(
+    "https://gpprfxmjjjowyuqgvwzr.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
+);
 
 // Sign In/Out
 async function signInOut() {
@@ -28,3 +32,6 @@ async function checkSession() {
     }
     return null;
 }
+
+// If offline set userid to null else check session
+window.userid = !navigator.onLine ? null : await checkSession();
