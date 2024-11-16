5039d6ff3cca5e296960c4a5bda4b70aef23d031
Umut Şen <umutsen2662@gmail.com>
2024-11-16 21:58:10 +0200

    possible fix on supabase

--- supabase.js
+++ supabase.js
@@ -1,13 +1,12 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-const supabase = await createClient(
+window.supabase = await createClient(
     "https://gpprfxmjjjowyuqgvwzr.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHJmeG1qampvd3l1cWd2d3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDU0NTQsImV4cCI6MjA0NjQyMTQ1NH0.e_RwbQVTsd4ggnH79bcX8gWL8o61pZ_wan5ypQjB77Q"
 );
-window.supabase = supabase;
 
 // Check if a user is signed in
 async function checkSession() {
-    const { data, error } = await supabase.auth.getUser();
+    const { data, error } = await window.supabase.auth.getUser();
 
     if (error) {
         console.error("Session Check Error:", error);
