3b0529f217894477af4053e1246c8139dee5e87e
Umut Şen <umutsen2662@gmail.com>
2024-11-16 21:09:45 +0200

    recording the exam list in local storage for offline use

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js");
 
-// Precache and route static files
-workbox.precaching.precacheAndRoute([
-    { url: "/Scheduler/", revision: null },
-    { url: "/Scheduler/index.html", revision: "1" },
-    { url: "/Scheduler/style.css", revision: "1" },
-    { url: "/Scheduler/manifest.json", revision: "1" },
-    { url: "/Scheduler/scheduler any.png", revision: "1" },
-    { url: "/Scheduler/scheduler maskable.png", revision: "1" },
-]);
+if (workbox) {
+    // Precache and route static files
+    workbox.precaching.precacheAndRoute([
+        { url: "/Scheduler/", revision: null },
+        { url: "/Scheduler/index.html", revision: "1" },
+        { url: "/Scheduler/style.css", revision: "1" },
+        { url: "/Scheduler/manifest.json", revision: "1" },
+        { url: "/Scheduler/scheduler any.png", revision: "1" },
+        { url: "/Scheduler/scheduler maskable.png", revision: "1" },
+    ]);
+}
