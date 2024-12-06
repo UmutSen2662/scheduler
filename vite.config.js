65ead187ec5e8226f2c486c071d35d0ecc0e5d07
Umut Şen <umutsen2662@gmail.com>
2024-12-06 19:07:37 +0200

    PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [svelte(), VitePWA()],
     base: "/Scheduler/",
     build: {
         target: "esnext", // Enables modern JavaScript features
+        outDir: "docs",
     },
 });
